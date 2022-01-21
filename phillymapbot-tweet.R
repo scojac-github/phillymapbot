44cd9a18822a93ab16a60f19f1033d5deccacc58
Scott Jacobson <scottjacobson@Scotts-Mac-mini.local>
2022-01-21 18:00:29 -0500

    Update

--- phillymapbot-tweet.R
+++ phillymapbot-tweet.R
@@ -6,8 +6,12 @@ phillymapbot_token <- rtweet::create_token(
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv = FALSE
 )
+
+rtweet::post_tweet(status = "This is a test tweet.",
+                   token = phillymapbot_token)
 
 # Generate random coordinates within specific limits
 lon <- round(runif(1, -75.2, -75.1), 4)
